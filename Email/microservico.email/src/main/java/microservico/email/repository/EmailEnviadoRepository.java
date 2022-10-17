591722c21cfc869b69799640e91bcfe086176c40
GuilhermeHASena <82185975+GuilhermeHASena@users.noreply.github.com>
2022-10-17 14:50:35 -0300

    Update CORS na controller de Envio de Emails

--- Email/microservico.email/src/main/java/microservico/email/repository/EmailEnviadoRepository.java
+++ Email/microservico.email/src/main/java/microservico/email/repository/EmailEnviadoRepository.java
@@ -1,9 +1,14 @@
 package microservico.email.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import microservico.email.models.EmailEnviadoModel;
 
 public interface EmailEnviadoRepository extends MongoRepository<EmailEnviadoModel, String>{
 
+	@Query("{dataEnvio: '?0'}")
+	Optional<EmailEnviadoModel> obterPorData(String data);
 }
